18d829c9b55330da15352a0c1aa461ef2f8c6de5
joaoschoen <joaoschoen@gmail.com>
2025-04-15 22:02:25 -0300

    Waypoints

--- src/app/model/areas/town.ts
+++ src/app/model/areas/town.ts
@@ -2,7 +2,7 @@ import { Area, AreaLevel } from "./area";
 
 export class Town extends Area{
 
-    constructor(id:string,name:string,level:AreaLevel,exits:string[]){
-        super(id,name,level,exits)
+    constructor(id:string,name:string,level:AreaLevel,has_waypoint:boolean,exits:string[]){
+        super(id,name,level,has_waypoint,exits)
     }
 }
