3c26a2e6935247f84c0981796027cae0450bbf4c
Victor Ross <vicjun22@gmail.com>
2025-04-28 01:16:24 -0300

    DC-50 | fix: fixing the new main menu

--- src/app/pages/home-page/home-page.component.ts
+++ src/app/pages/home-page/home-page.component.ts
@@ -1,19 +1,23 @@
 import { CommonModule } from '@angular/common';
-import { Component, OnInit } from '@angular/core';
+import { Component, OnDestroy } from '@angular/core';
 import { Router, RouterModule } from '@angular/router';
+import { Subject } from 'rxjs';
 import { ButtonComponent } from '../../ui/button/button.component';
 
 @Component({
   selector: 'app-home-page',
   imports: [CommonModule, ButtonComponent, RouterModule],
   templateUrl: './home-page.component.html',
-  styleUrl: './home-page.component.css'
 })
-export class HomePageComponent implements OnInit {
+export class HomePageComponent implements OnDestroy {
+
+  public destroy$ = new Subject<boolean>();
     
   public constructor(private router: Router) { }
-  ngOnInit(): void {
-    throw new Error('Method not implemented.');
+  
+  public ngOnDestroy(): void {
+    this.destroy$.next(true);
+    this.destroy$.complete();
   }
 
   public singlePlayer() {
--- src/app/ui/button/button.component.ts
+++ src/app/ui/button/button.component.ts
@@ -1,11 +1,11 @@
 import { CommonModule } from '@angular/common';
 import { Component, EventEmitter, Input, Output } from '@angular/core';
+import { Subject } from 'rxjs';
 
 @Component({
   selector: 'app-button',
   imports: [CommonModule],
   templateUrl: './button.component.html',
-  styleUrl: './button.component.css'
 })
 export class ButtonComponent {
 
@@ -14,8 +14,15 @@ export class ButtonComponent {
   @Input() disabled: boolean = false;
   @Output() buttonClick = new EventEmitter<any>(false);
 
+  public destroy$ = new Subject<boolean>();
+
   public onClick(event: Event) {
     event.preventDefault();
     this.buttonClick.emit(true);
+  }
+
+  public ngOnDestroy(): void {
+    this.destroy$.next(true);
+    this.destroy$.complete();
   }
 }
